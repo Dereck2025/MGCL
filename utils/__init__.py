e931f8b22909a04af64d9a239c8c3357db873b24
Dereck2025 <68171118+Dereck2025@users.noreply.github.com>
2025-06-09 16:28:16 +0800

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,25 @@
+from .yaml_config_hook import yaml_config_hook
+from .save_model import save_model
+
+
+class IterLoader:
+    def __init__(self, loader, length=None):
+        self.loader = loader
+        self.length = length
+        self.iter = None
+
+    def __len__(self):
+        if self.length is not None:
+            return self.length
+
+            return len(self.loader)
+
+    def new_epoch(self):
+        self.iter = iter(self.loader)
+
+    def next(self):
+        try:
+            return next(self.iter)
+        except:
+            self.iter = iter(self.loader)
+            return next(self.iter)

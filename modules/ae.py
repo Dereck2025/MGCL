0a242fa2f6dddc62e9b75f0a45a78b7dbbfaa511
Dereck2025 <68171118+Dereck2025@users.noreply.github.com>
2025-08-28 17:10:45 +0800

    Update ae.py

--- modules/ae.py
+++ modules/ae.py
@@ -7,51 +7,45 @@ import numpy as np
 import os
 
 
-
-
-def block(in_c,out_c):
-    layers=[
-        nn.Linear(in_c,out_c),
+def block(in_c, out_c):
+    layers = [
+        nn.Linear(in_c, out_c),
         nn.ReLU(True)
     ]
     return layers
 
-class Encoder(nn.Module):
-    def __init__(self,input_dim=8518,inter_dims=[5000,2000,1000,256]):
-        super(Encoder,self).__init__()
 
-        self.encoder=nn.Sequential(
+class Encoder(nn.Module):
+    def __init__(self, input_dim, inter_dims=[5000, 2000, 1000, 256]):
+        super(Encoder, self).__init__()
+
+        self.encoder = nn.Sequential(
             nn.Dropout(),
-            *block(input_dim,inter_dims[0]),
-            *block(inter_dims[0],inter_dims[1]),
-            *block(inter_dims[1],inter_dims[2]),
-            *block(inter_dims[2],inter_dims[3])  
+            *block(input_dim, inter_dims[0]),
+            *block(inter_dims[0], inter_dims[1]),
+            *block(inter_dims[1], inter_dims[2]),
+            *block(inter_dims[2], inter_dims[3])
         )
 
-
     def forward(self, x):
-        z=self.encoder(x)
+        z = self.encoder(x)
 
         return z
 
 
-
-
-
 class AE(nn.Module):
-    def __init__(self,hid_dim=256):
-        super(AE,self).__init__()
-        self.encoder=Encoder()
-        self.decoder=Decoder()
+    def __init__(self, input_dim,hid_dim=256):
+        super(AE, self).__init__()
+        self.encoder = Encoder(input_dim=input_dim, inter_dims=[5000, 2000, 1000, hid_dim])
+        #self.decoder = Decoder()
 
         self.rep_dim = hid_dim
 
     def forward(self, x):
         z = self.encoder(x)
-        x_out = self.decoder(z)
-        
+        #x_out = self.decoder(z)
+
         return z
 
 
 
-    
